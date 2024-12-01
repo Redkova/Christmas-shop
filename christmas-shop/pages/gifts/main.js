a8ca90bffb0224f608d7cf56708367a506f1b67d
Alexandra Redkova <sandraredkova@gmail.com>
2024-12-01 03:39:34 +0100

    feat: add animation for burger menu at gifts page

--- christmas-shop/pages/gifts/main.js
+++ christmas-shop/pages/gifts/main.js
@@ -8,4 +8,29 @@
         menu.classList.toggle('active');
         body.classList.toggle('body-lock');
     });
-    
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            const href = link.getAttribute('href');
+
+            if (href.includes('#')) {
+                e.preventDefault();
+            }
+
+            const linkID = href.split('#')[1];
+            const linkSection = document.getElementById(linkID);
+            
+            if (linkSection) {
+                linkSection.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                })
+            }
+
+            burgerItem.classList.remove('active');
+            menu.classList.remove('active');
+            body.classList.remove('body-lock');
+        });
+    });
+
+})();
