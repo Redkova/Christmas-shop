28519fdac643cd2a13e2a5bcd7817e7b9860a26c
Alexandra Redkova <sandraredkova@gmail.com>
2024-12-02 22:11:07 +0100

    feat: implement random gift card display at gifts page

--- christmas-shop/pages/gifts/main.js
+++ christmas-shop/pages/gifts/main.js
@@ -59,4 +59,12 @@
     window.addEventListener('scroll', scrollButton);
 })();
 
+(function randomCards() {
+    const cardsContainer = document.querySelector('.gifts-cards');
+    const cards = document.querySelectorAll('.best-gft-card');
 
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        cardsContainer.appendChild(cards[j]);
+    }
+})();
